059691fd17b68a54b3740a63bd052e4828ba445c
Joli Holmes <holmesjoli@gmail.com>
2022-06-24 13:50:52 -0400

    fixed bugs with selector

--- R/scrape.R
+++ R/scrape.R
@@ -14,14 +14,14 @@ scrape_links <- function(endpoint) {
 #' @title Scrape element
 #' @description Returns the text from a specific element in the OHRI database
 #' @return string
-scrape_element <- function(page, i = NULL, selector = "#tblHealthy > tbody > tr:nth-child({i}) > td.top.dbfield") {
-
+scrape_element <- function(page, i = NULL, selector = "#tblHealthy > tbody > tr:nth-child({i}) > td.dbfield") {
+  
   if (!is.null(i)) {
-    selector <- glue::glue(selector, i) 
+    selector <- glue::glue(selector, i = i)
   }
 
   x <- page %>%
-    rvest::html_node(selector) %>%
+    rvest::html_nodes(selector) %>%
     rvest::html_text()
 
   if (length(x) == 0) {
@@ -40,21 +40,25 @@ scrape_element <- function(page, i = NULL, selector = "#tblHealthy > tbody > tr:
 #' @param links a list of the links to iterate through
 #' @return data.frame
 scrape_data <- function(endpoint, links) {
+  
   lapply(links, function(x) {
     
     page <- xml2::read_html(glue::glue(endpoint, x)) %>%
       rvest::html_nodes("#content")
     
+    # browser()
+
     data.frame(title = scrape_element(page, selector = "#tblHealthy > thead > tr > th.top.dbfield"),
-               opts = scrape_element(page, 2),
-               updated = scrape_element(page, 3),
-               format = scrape_element(page, 4),
-               artifact_link = scrape_element(page, 5),
-               designer = scrape_element(page, 6),
-               developer = scrape_element(page, 7),
-               condition = scrape_element(page, 8),
-               deliberation_point = scrape_element(page, 9),
-               language = scrape_element(page, 10)
+               audience = scrape_element(page, i = 1),
+               opts = scrape_element(page, i = 2),
+               updated = scrape_element(page, i = 3),
+               format = scrape_element(page, i = 4),
+               artifact_link = scrape_element(page, i = 5),
+               designer = scrape_element(page, i = 6),
+               developer = scrape_element(page, i = 7),
+               condition = scrape_element(page, i = 8),
+               deliberation_point = scrape_element(page, i = 9),
+               language = scrape_element(page, i = 10)
     )
   }) %>% 
     dplyr::bind_rows()
